4059fb6a63eeedc1b43dd06b35f263b74e40494f
guofeihong <1826252461@qq.com>
2024-05-10 14:26:03 +0800

    线上打包路径配置

--- vite.config.ts
+++ vite.config.ts
@@ -20,7 +20,7 @@ export default defineConfig(({ command }) => {
         localEnabled: command === "serve",
       }),
     ],
-    base: './panda-vue3', // 打包的相对路径
+    base: '/panda-vue3/', // 打包的相对路径
     resolve: {
       alias: {
         "@": path.resolve("./src"), // 相对路径别名配置，使用 @ 代替 src
